7e6c7b1a087dde9fcb01585ac84c808769d6ba7e
Yunyi Zhu <yunyizhu@mit.edu>
2025-01-07 14:19:28 +0300

    folder reorg - connect to student portfolio pages

--- js/student.js
+++ js/student.js
@@ -6,16 +6,16 @@ document.addEventListener("DOMContentLoaded", async () => {
     const student = students.find(s => s.id === studentId);
     if (student) {
         document.getElementById("student-name").innerText = student.name;
-        document.getElementById("student-photo").src = "assets/student_images/"+ studentId + "_profile.png" || "assets/student_images/placeholder.png";
+        document.getElementById("student-photo").src = "assets/student_headshots/"+ studentId + ".jpg" || "assets/student_images/placeholder.png";
         document.getElementById("student-photo").alt = student.name;
         document.getElementById("student-bio").innerText = student.bio;
 
         // Populate Lab 1 and Lab 2 information
-        document.getElementById("lab1-image").src = "assets/student_images/"+ studentId + "_lab1.png" || "assets/student_images/placeholder.png";
+        document.getElementById("lab1-image").src = "assets/lab_images/lab1/"+ studentId + "_lab1.png" || "assets/student_images/placeholder.png";
         document.getElementById("lab1-image").alt = "Lab 1";
         document.getElementById("lab1-description").innerText = student.lab1;
 
-        document.getElementById("lab2-image").src = "assets/student_images/"+ studentId + "_lab2.png" || "assets/student_images/placeholder.png";
+        document.getElementById("lab2-image").src = "assets/lab_images/lab2/"+ studentId + "_lab2.png" || "assets/student_images/placeholder.png";
         document.getElementById("lab2-image").alt = "Lab 2";
         document.getElementById("lab2-description").innerText = student.lab2;
     } else {
